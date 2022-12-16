8232f9104a6f2da502bb1fef5eae1a07e13ebf87
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-12-16 20:12:39 +0300

    Deleting extra fork()

--- 2HWH/task_runsim/main.c
+++ 2HWH/task_runsim/main.c
@@ -4,60 +4,88 @@ int number_active_cmd = 0;
 
 int main(char argc, char *argv[])
 {
-    signal(SIGCHLD, handler);
-    // signal(SIGINT, handler_kill_all_process);
+    int limit_numb = 0;
+    if (argc == 1)
+    {
+        printf("Number of active process is set by default: %d\n", OPTIMAL_NUMBER_OF_CMD);
+        limit_numb = OPTIMAL_NUMBER_OF_CMD;
+    }
+    else
+        limit_numb = getnumb(argv[1]);
 
-    for (int limit_numb = getnumb(argv[1]); true;)
-        execute(limit_numb);
+    char *cmd = (char *)calloc(MAXLEN, sizeof(char));
+
+    for (; true;)
+    {
+        getcmd(&cmd);
+
+        if (number_active_cmd == limit_numb)
+        {
+            printf("Sorry, the limit of active process has been approached. Wait a minute\n");
+            continue;
+        }
+        else
+        {
+            printf("Command: \"%s\"\n", cmd);
+            number_active_cmd++;
+            printf("Number of active program: %d\n", number_active_cmd);
+            execute(cmd);
+        }
+        memset(cmd, '\0', MAXLEN);
+        fflush(stdin);
+    }
+
+    free(cmd);
     return 0;
 }
 
 void handler(int signal)
 {
     if (signal == SIGCHLD)
-        --number_active_cmd; //  reducing number of active processes
-}
-
-void handler_kill_all_process(int signal)
-{
-    pid_t pid = getpid();
-    if (signal == SIGINT)
-        kill(-pid, SIGKILL); //  sending signal of killing all of process in the group
-}
-
-int execute(int limit_numb)
-{
-    printf("NUMBER OF ACTIVE PROGRAM: %d\n", number_active_cmd);
-
-    //  before all of this staff we should understand number of active commands
-    if (number_active_cmd == limit_numb)
     {
-        printf("Sorry, the limit of active process at the same time was approached. Wait a minute\n");
-        waitpid(0, NULL, 0); //  waiting for a process which has finished his work
+        number_active_cmd--; //  reducing number of active processes
+        printf("Number of active program: %d\n\n", number_active_cmd);
     }
-    //  after checking
+}
 
-    char *cmd = 0;
-    getcmd(&cmd);
-    ++number_active_cmd;
+int execute(const char *cmd)
+{
+    signal(SIGCHLD, handler);
 
-    pid_t res = fork();
+    pid_t aux_res = fork();
 
-    if (res < 0)
+    if (aux_res < 0)
     {
         printf("Can't create child's process\n");
         exit(-1);
     }
-    else if (res > 0) //  inside the parent's process
+    else if (aux_res == 0)
     {
-        int status;
-        wait(&status);
-        delcmd(cmd);
+        int res_exec = execl("/bin/bash", "/bin/bash", "-c", cmd, NULL);
+        if (res_exec < 0)
+            printf("Error execute\n");
     }
-    else //  inside the child's process
+}
+
+int getnumb(const char *str)
+{
+    int numb = 0;
+    for (int i = 0, strsize = strlen(str); i < strsize; i++)
     {
-        printf("NUMBER OF ACTIVE PROGRAM: %d\n", number_active_cmd);
-        // execl(cmd, cmd, NULL); //  execute a program with infinite circle
-        execl("/bin/bash", "/bin/bash", "-c", cmd, NULL);
+        numb += str[i] - '0';
+        numb *= 10;
     }
+    return numb / 10;
+}
+
+int getcmd(char **cmd)
+{
+    char symb = getc(stdin);
+    for (char i = 0; (symb != EOF) && (symb != '\n') && (i < MAXLEN); i++)
+    {
+        (*cmd)[i] = symb;
+        symb = getc(stdin);
+    }
+    if (symb == EOF)
+        kill(-getpid(), SIGINT);
 }
